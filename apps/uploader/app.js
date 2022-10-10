3f706f7bedb713bcfadcaff174315eafc1ccb85d
JunkyByte <adriano.donninelli@hotmail.it>
2022-10-11 01:59:44 +0200

    up

--- apps/uploader/app.js
+++ apps/uploader/app.js
@@ -1,13 +1,19 @@
+require("Font8x12").add(Graphics);
+
 function swap() {
+  g.clear()
+  g.drawString('Restarting BT!', 30, 30)
   NRF.sleep();
 
   setTimeout(() => {
     NRF.wake();
-  }, 500)
+    g.clear()
+    g.setFont("8x12");
+    g.drawString('Done!', 30, 30)
+    g.drawString('Button to restart BT', 30, 50)
+  }, 750)
 
-  g.clear()
-  g.setFont("8x12");
-  g.drawString('Bluetooth state:', 30, 30)
 }
 
+swap()
 setWatch(swap, BTN1, {repeat:true, edge:"rising"});
