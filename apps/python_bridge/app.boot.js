cdd80658cf5bf1569e06fa10d21ef34f752bf0c9
JunkyByte <adriano.donninelli@hotmail.it>
2022-10-15 18:05:09 +0200

    updates

--- apps/python_bridge/app.boot.js
+++ apps/python_bridge/app.boot.js
@@ -1,5 +1,3 @@
-var btdatareceiver = new Object();
-
 function send_data(data) {
   // This will be called from python and emitted here as an event
   btdatareceiver.emit('python_bridge', data);
@@ -7,6 +5,7 @@ function send_data(data) {
 
 function send_state(state, value) {
   // This is used to alert device of system states
+  Bluetooth.println('received ' + state + ' ' + value);
   btdatareceiver.emit(state, value);
 }
 
@@ -21,29 +20,69 @@ function send_to_python(task, msg, options) {
   Bluetooth.println(prefix + JSON.stringify(d));
 }
 
-function wait_connection(){
-  var interval = setInterval(() => {
+function wait_connection(from_notification, show_messages){
+  if (from_notification === undefined)
+    from_notification = false
+  if (show_messages === undefined)
+    show_messages = false
+
+  btdatareceiver.on('disconnect', function(){
+    if (show_messages)
+      E.showMessage('Disconnected', 'python_bridge');
     NRF.sleep();
     setTimeout(() => {
       NRF.wake();
-    }, 100);
-  }, 3000);
-  
-  return new Promise(function(resolve, reject){
-    btdatareceiver.on('connect', (data) => {
-      send_to_python('connect', '');
-      clearInterval(interval);
-      resolve();
-    });
+    }, 1000);
   });
+
+  // We might be connected already
+  // Check if the connection is python
+  function new_conn() {
+    NRF.disconnect();
+    var dc_interval = setInterval(() => {
+      NRF.disconnect();
+    }, 10000);
+  
+    return new Promise(function(resolve, reject){
+      var rej_interval = setTimeout(() => {
+        reject('timeout')
+      }, 30000);
+
+      btdatareceiver.on('connect', (data) => {
+        clearInterval(dc_interval);
+        clearInterval(rej_interval);
+        send_to_python('connect', '');
+        resolve();
+      });
+    });
+  }
+
+  if (NRF.getSecurityStatus().connected && !from_notification) {  // If we have bluetooth connection we send check_connection
+    if (show_messages)
+      E.showMessage('Checking if connection is python', 'python_bridge');
+    send_to_python('check_connection', '');
+    var check_conn = setTimeout(() => {  // If nothing happen we create new connection
+      return new_conn()
+    }, 10000);  // We give python a certain time to answer us then create a new connection
+
+    return new Promise(function(resolve, reject){
+      btdatareceiver.on('check_connection', (data) => {  // If we receive answer we are already connected
+        Bluetooth.println('python said we are connected');
+        clearInterval(check_conn);
+        resolve();
+      })
+    });
+  } else {
+    if (show_messages)
+      E.showMessage('Starting new connection', 'python_bridge');
+    return new_conn()
+  }
 }
 
 
 // Override Bangle.http requests, python will manage them
 Bangle.http = function (url, options) {
   options = options||{};
-  // if (!NRF.getSecurityStatus().connected)  // TODO this makes no sense in this situation
-  //   return Promise.reject("Not connected to Bluetooth");
   if (Bangle.httpRequest === undefined)
     Bangle.httpRequest={};
   if (options.id === undefined) {
@@ -54,11 +93,11 @@ Bangle.http = function (url, options) {
   }
 
   // The actual change
-  return wait_connection().then(() => {
-    // E.showMessage('sending to python');
+  return btdatareceiver.wait_connection().then(() => {
+    // E.showMessage('Sending http request to python', 'python_bridge');
     send_to_python('http', url, options);
     return new Promise(function(resolve, reject){
-      // E.showMessage('http response waiting');
+      // E.showMessage('Waiting http response...', 'python_bridge');
       var rej_interval = setInterval(() => {
         reject('timeout')
       // }, options.timeout || 30000);  // TODO  This must be a pretty long timeout!
@@ -75,6 +114,9 @@ Bangle.http = function (url, options) {
         clearInterval(rej_interval);
         response = JSON.parse(response);
 
+        if (response.type === 'json')  // TODO: This is needed to go back to json string to be parsed if original was json
+          response.resp = JSON.stringify(response.resp)
+
         //get the promise and call the promise resolve
         if (Bangle.httpRequest === undefined) return;
         var request=Bangle.httpRequest[response.id];
@@ -89,3 +131,23 @@ Bangle.http = function (url, options) {
     });
   });
 }
+
+
+var btdatareceiver = new Object();
+btdatareceiver.wait_connection = wait_connection
+// Add a check on pythonista notifications to set to connectable mode the device
+E.on("notify", function(event){ 
+  if (event.preExisting){
+    return
+  }
+
+  if (event.appId !== 'com.omz-software.Pythonista3'){
+    return
+  }
+
+  if (event.message === 'connect'){
+    btdatareceiver.wait_connection(from_notification=true, show_messages=true).then(() => {
+      E.showMessage('connected')
+    });
+  }
+});
--- apps/python_bridge/app.js
+++ apps/python_bridge/app.js
@@ -1,5 +1,5 @@
 // Connect to python script by resetting bluetooth state until we receive a connection event
 E.showMessage('Waiting for connection');
-wait_connection().then(() => {
+wait_connection(show_messages=true).then(() => {
   E.showMessage('Connected!')
 });
